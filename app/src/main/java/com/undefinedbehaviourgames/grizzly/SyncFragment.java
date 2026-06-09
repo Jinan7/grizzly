1b8401bfec0d63b9367beb4c92d37d4706038c23
Jinan <lakanj25@gmail.com>
2026-06-09 04:16:38 +0100

    jun 9 04:16

--- app/src/main/java/com/undefinedbehaviourgames/grizzly/SyncFragment.java
+++ app/src/main/java/com/undefinedbehaviourgames/grizzly/SyncFragment.java
@@ -9,12 +9,15 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SyncFragment extends Fragment {
 
     private static final String ARG_SYNC_TO = "sync_to";
     private static final String TAG = "SyncFragment";
 
+    private RecyclerView mRecyclerView;
     public static SyncFragment newInstance(String [] syncTo) {
 
         SyncFragment fragment = new SyncFragment();
@@ -41,6 +44,36 @@ public class SyncFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_syncs, container, false);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.syncs_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerView.setAdapter(new SyncAdapter());
         return v;
+    }
+
+
+    private class SyncHolder extends RecyclerView.ViewHolder {
+        public SyncHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    private class SyncAdapter extends RecyclerView.Adapter<SyncHolder> {
+
+        @NonNull
+        @Override
+        public SyncHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(getContext()).inflate(R.layout.component_sync_progress_layout, parent, false);
+            return new SyncHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull SyncHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 10;
+        }
     }
 }
